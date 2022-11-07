19c4a3178a53085c997e267548d62d821ebd9ba3
darikmohammed <r.m.darik@gmail.com>
2022-11-08 00:30:59 +0300

    Add sun light to the scene

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,15 +1,17 @@
 import * as THREE from 'three';
 
 import Experience from '../Experience.js';
+import Environment from './Environment.js';
 
 export default class World {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+    this.enivronment = new Environment();
 
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ wireframe: true })
+      new THREE.MeshStandardMaterial()
     );
     this.scene.add(testMesh);
   }
